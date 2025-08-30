7c6707ed560a1dff49bfc0cdbc255e8ce590148f
Pranjal <versetechno007@gmail.com>
2025-08-30 14:34:17 +0530

    fix: Integrate client folder into main repository

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div style={{marginTop:46}} className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
